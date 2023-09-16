d7f16270f3ff52eab599b9c53aee2f67cb0351a6
Виталий Лигай <vit-ligay04@mail.ru>
2023-09-16 17:50:31 +0300

    Redy RPG module

--- test_file.py
+++ test_file.py
@@ -1,8 +1,5 @@
 from random import randint
 
-from graphic_arts.start_game_banner import run_screensaver
-
-
 DEFAULT_ATTACK = 5
 DEFAULT_DEFENCE = 10
 DEFAULT_STAMINA = 80
@@ -29,7 +26,7 @@ class Character:
         return (f'{self.name} блокировал {value_defence} урона')
 
     def special(self):
-        return (f'{self.name} применил специальное умение'
+        return (f'{self.name} применил специальное умение '
                 f'{self.SPECIAL_SKILL} {self.SPECIAL_BUFF}»')
 
     def __str__(self):
@@ -118,14 +115,5 @@ def choice_char_class(char_name: str) -> Character:
     return char_class
 
 
-if __name__ == '__main__':
-    run_screensaver()
-    print('Приветствую тебя, искатель приключений!')
-    print('Прежде чем начать игру...')
-    char_name: str = input('...назови себя: ')
-    print(f'Здравствуй, {char_name}! '
-          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
-    print('Ты можешь выбрать один из трёх путей силы:')
-    print('Воитель, Маг, Лекарь')
-    char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
+choice_char_class('Кодослав')
+start_training(warrior)
